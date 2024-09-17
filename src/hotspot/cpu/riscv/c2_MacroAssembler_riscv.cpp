675348c1dc720869755f748e62b49188efe3fc24
hamlin <hamlin@hamlin.eu.rivosinc.com>
2024-09-17 14:44:35 +0000

    fix issue

--- src/hotspot/cpu/riscv/c2_MacroAssembler_riscv.cpp
+++ src/hotspot/cpu/riscv/c2_MacroAssembler_riscv.cpp
@@ -3032,6 +3032,6 @@ void C2_MacroAssembler::load_nklass_compact_c2(Register dst, Address src) {
   // comes as obj-start in obj and klass_offset_in_bytes in disp.
   assert(UseCompactObjectHeaders, "must");
   int offset = oopDesc::mark_offset_in_bytes() - oopDesc::klass_offset_in_bytes();
-  lw(dst, src.plus(offset));
+  ld(dst, src.plus(offset));
   srli(dst, dst, markWord::klass_shift);
 }
